8c683730ca4a76951f98eff27d6358d91ce5358d
marcio heleno <marcio012@edu.unifor.br>
2019-11-23 00:02:32 -0300

    configuração incial babel, eslint, husky

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import '@babel/polyfill'
+import http from 'http'
+
+const requestHandler = (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World, eslint fun fun!!!');
+};
+
+const server = http.createServer(requestHandler);
+server.listen(3000);
