050aeeb012aac5c9bea2b73b607bc789c2b9c8db
marcio heleno <marcio012@edu.unifor.br>
2019-11-23 00:03:13 -0300

    ajuste no .gitignore

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import '@babel/polyfill'
-import http from 'http'
+import '@babel/polyfill';
+import http from 'http';
 
 const requestHandler = (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
